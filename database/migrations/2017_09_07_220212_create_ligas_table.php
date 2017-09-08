725aba7bc48b09fad1c4823fceda99cba0670c76
Philipe Almeida <philipe.almeida@outlook.com>
2017-09-08 01:17:49 +0000

    Criação das tabelas de Times

---
+++ database/migrations/2017_09_07_220212_create_ligas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLigasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ligas',function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('version_id')->unsigned();
+            $table->foreign('version_id')->references('id')->on('versions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ligas');
+    }
+}
